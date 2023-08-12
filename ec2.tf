d13f346ca49b001f4c9b34b1980135fee54931ae
rwirba1 <130412758+rwirba1@users.noreply.github.com>
2023-08-12 09:47:03 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-1" # Change this to your desired AWS region
 }
 
-resource "aws_security_group" "prod_sg_new" {
-  name        = "prod_sg_new"
+resource "aws_security_group" "prod_sg" {
+  name        = "prod_sg"
   description = "Allow inbound traffic on port 80 and 22"
 
   ingress {
@@ -32,14 +32,14 @@ resource "aws_security_group" "prod_sg_new" {
   }
 
   tags = {
-    Name = "prod_sg_new"
+    Name = "prod_sg"
   }
 }
 
 resource "aws_instance" "prod_web_server" {
   ami             = "ami-053b0d53c279acc90" # This AMI ID corresponds to Ubuntu 18.04 in us-west-2. Find the correct AMI for your desired Ubuntu version and region.
   instance_type   = "t2.micro" # Choose the desired instance type.
-  security_groups = [aws_security_group.prod_sg_new.name]
+  security_groups = [aws_security_group.prod_sg.name]
   key_name        = "mykey" # Use the name of your EC2 Key Pair
 
   tags = {
