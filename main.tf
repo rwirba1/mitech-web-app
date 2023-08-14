566e36cecb3157647582e240267e767b2264e1c5
rwirba1 <130412758+rwirba1@users.noreply.github.com>
2023-08-14 02:39:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,8 +98,8 @@ resource "aws_security_group" "lb_sg" {
   name_prefix = "mt-web-prod-lb-sg"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 0
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
