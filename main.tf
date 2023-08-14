2dc8330eb91134e2eee34060679cb25bb9924db2
rwirba1 <130412758+rwirba1@users.noreply.github.com>
2023-08-14 03:40:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -106,6 +106,12 @@ resource "aws_security_group" "lb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
 }
   tags = {
     Name = "mt-prod-lb-sg"
