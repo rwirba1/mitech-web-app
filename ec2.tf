72d42845724a556f492ac7d42c5b24dac5452a7b
rwirba1 <130412758+rwirba1@users.noreply.github.com>
2023-09-16 04:27:09 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "prod_web_server" {
   ami             = "ami-053b0d53c279acc90" # This AMI ID corresponds to Ubuntu 18.04 in us-west-2. Find the correct AMI for your desired Ubuntu version and region.
   instance_type   = "t2.micro" # Choose the desired instance type.
   security_groups = [aws_security_group.prod_sg.name]
-  key_name        = "mykey" # Use the name of your EC2 Key Pair
+  key_name        = "ryankey" # Use the name of your EC2 Key Pair
 
   tags = {
     Name = "prod_web_server"
