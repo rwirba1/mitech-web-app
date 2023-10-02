f9bc2e0f96569be3c84a9fcd1836dfa643a6bcd6
admin <admin@gmail.com>
2023-10-01 20:17:14 -0500

    modified:   output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "instance_public_ip" {
   value = aws_instance.demo_app.public_ip
 }
-output "s3_name" {
+output "aws_s3_bucket_name" {
   value = aws_s3_bucket.name
 }
-output "s3_arn" {
+output "aws_s3_bucket_arn" {
   value = aws_s3_bucket.arn
 }
